2d8910066d863dd46ace0f766596d16524b43bdf
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-20 16:43:32 -0300

    Resolução do requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalsOfZoo = {};
+    data.species.forEach((animalType) => {
+      animalsOfZoo[animalType.name] = animalType.residents.length;
+    });
+    return animalsOfZoo;
+  }
+  const mySpecie = data.species.find((specie) => specie.name === animal.specie)
+    .residents;
+  if (animal.sex) {
+    return mySpecie.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return mySpecie.length;
 }
 
 module.exports = countAnimals;
