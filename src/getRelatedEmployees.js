082de8b8e64d5907d894ef67d8f5746321881d28
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-20 16:03:42 -0300

    Resolução do requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = [];
+  data.employees.forEach((person) => {
+    person.managers.forEach((myManager) => {
+      if (!managers.includes(myManager)) managers.push(myManager);
+    });
+  });
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const myPersonsToManager = [];
+    data.employees.forEach((person) => {
+      if (person.managers.includes(managerId)) {
+        myPersonsToManager.push(`${person.firstName} ${person.lastName}`);
+      }
+    });
+    return myPersonsToManager;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
