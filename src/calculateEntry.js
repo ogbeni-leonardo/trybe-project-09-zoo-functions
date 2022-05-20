50c6152e0098dc459646ce2d4d4fc74b2e9cc0f0
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-20 17:45:01 -0300

    Resolução do requisito 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let [child, adult, senior] = [0, 0, 0];
+  entrants.forEach((person) => {
+    if (person.age < 18) child += 1;
+    else if (person.age >= 18 && person.age < 50) adult += 1;
+    else senior += 1;
+  });
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  try {
+    let total = 0;
+    const entrantsOfZoo = countEntrants(entrants);
+    Object.keys(entrantsOfZoo).forEach((group) => {
+      if (entrantsOfZoo[group] !== 0) {
+        total += data.prices[group] * entrantsOfZoo[group];
+      }
+    });
+    return total;
+  } catch (error) { return 0; }
 }
+
+const entrants = [
+  { name: 'zeca', age: 5 },
+  { name: 'pagodinho', age: 50 },
+];
+
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
