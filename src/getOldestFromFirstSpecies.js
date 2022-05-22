6c81641325c0ff5eae17c2b3a59b95ec07729a5b
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-21 22:18:47 -0300

    Refatoração do requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,16 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const person = data.employees.find((myPerson) => myPerson.id === id);
-  const specie = person.responsibleFor[0];
-  const getAnimals = data.species.find((mySpecie) => mySpecie.id === specie).residents;
-  const oldestAnimal = getAnimals.reduce((acc, curr) => {
-    if (acc.age < curr.age) {
-      return curr;
-    }
-    return acc;
-  });
-  return Object.values(oldestAnimal);
+  const employee = data.employees.find((person) => person.id === id);
+  return Object.values(data.species.find(
+    (specie) => specie.id === employee.responsibleFor[0],
+  ).residents.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr)));
 }
 
 module.exports = getOldestFromFirstSpecies;
