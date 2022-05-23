942b922c6ea5f8649d036e24bd8755adade3e736
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-23 18:26:32 -0300

    Refatoração do requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const myPerson = data.employees.find(
+  const employee = data.employees.find(
     (person) => person.firstName === employeeName || person.lastName === employeeName,
   );
-  return (myPerson !== undefined) ? myPerson : {};
+  return employee || {};
 }
 
 module.exports = getEmployeeByName;
