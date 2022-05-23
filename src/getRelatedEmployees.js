50f915bb65095f4c28dbe24d7bcc16704b6e9c1d
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-23 18:50:05 -0300

    Refatoração do requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,24 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  const managers = [];
-  data.employees.forEach((person) => {
-    person.managers.forEach((myManager) => {
-      if (!managers.includes(myManager)) managers.push(myManager);
-    });
-  });
-  return managers.includes(id);
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    const myPersonsToManager = [];
-    data.employees.forEach((person) => {
-      if (person.managers.includes(managerId)) {
-        myPersonsToManager.push(`${person.firstName} ${person.lastName}`);
-      }
-    });
-    return myPersonsToManager;
+    return employees.filter((employee) => employee.managers.includes(managerId))
+      .map((person) => `${person.firstName} ${person.lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
